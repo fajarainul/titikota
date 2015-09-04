b50ec9a912d86475383e70f743c53c8e26c8bf93
Fajar Ainul <fajarainul14@gmail.com>
2015-09-04 20:02:55 +0700

    inisialisasi

---
+++ titikota/app/src/main/java/com/fajarainul/coconut_dev/titikota/SetTimeActivity.java
@@ -0,0 +1,63 @@
+package com.fajarainul.coconut_dev.titikota;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class SetTimeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_time);
+
+        Button button = (Button) findViewById(R.id.search);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fetch fetch = new Fetch(getApplicationContext());
+                fetch.execute();
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_set_time, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void processAll(View view){
+
+    }
+
+    public void config(){
+
+    }
+
+
+
+}
